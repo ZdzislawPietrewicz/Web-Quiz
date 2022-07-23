28e65555aaa7d75dd05a3a435e500428f6586bfb
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-07-23 11:01:36 +0200

    added simple @PostMapping to check users anserws

--- engine/controller/QuizController.java
+++ engine/controller/QuizController.java
@@ -1,8 +1,10 @@
 package engine.controller;
 
 import engine.model.Quiz;
+import engine.model.QuizAnswer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +20,11 @@ public class QuizController {
         return quizList;
     }
 
+    @PostMapping("/api/quiz")
+    public QuizAnswer solveQuiz(@RequestParam int answer) {
+        if(answer==2){
+            return new QuizAnswer(true, "Congratulations, you're right!");
+        } else
+            return new QuizAnswer(false,"Wrong answer! Please, try again.");
+    }
 }
