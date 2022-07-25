4e8e88e1fe9671e68c0ece949cbf23786014c610
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-07-25 12:46:35 +0200

    @GetMapping & @PostMapping at /api/quizzes

--- engine/controller/QuizController.java
+++ engine/controller/QuizController.java
@@ -2,29 +2,46 @@ package engine.controller;
 
 import engine.model.Quiz;
 import engine.model.QuizAnswer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import engine.model.QuizQuestion;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class QuizController {
-    private final List<Quiz> quizList = List.of(
-            new Quiz("The Java Logo", "What is depicted on the Java logo?",
-                    new String[]{"Robot", "The Leaf", "Cup of coffee", "Bug"}));
+    private final List<Quiz> quizList = new ArrayList<>();
+    private final List<QuizQuestion> quizQuestionList = new ArrayList<>();
+
+    @PostMapping("/api/quizzes")
+    private QuizQuestion createNewQuizQuestion(@RequestBody Quiz quiz) {
+        QuizQuestion quizQuestion = new QuizQuestion(quizList.size() + 1, quiz.getTitle(),
+                quiz.getText(), quiz.getOptions());
+        quizList.add(quiz);
+        quizQuestionList.add(quizQuestion);
+        return quizQuestion;
+    }
+
+    @GetMapping("/api/quizzes")
+    private List<QuizQuestion> getAllQuizzes() {
+        return quizQuestionList;
+    }
+
+    @GetMapping("/api/quizzes/{id}")
+    private QuizQuestion getQuizQuestion(@PathVariable int id) {
+        return quizQuestionList.get(id - 1);
+    }
 
     @GetMapping("/api/quiz")
-    public List<Quiz> getQuizList() {
-        return quizList;
+    public void getQuizList() {
+
     }
 
     @PostMapping("/api/quiz")
     public QuizAnswer solveQuiz(@RequestParam int answer) {
-        if(answer==2){
+        if (answer == 2) {
             return new QuizAnswer(true, "Congratulations, you're right!");
         } else
-            return new QuizAnswer(false,"Wrong answer! Please, try again.");
+            return new QuizAnswer(false, "Wrong answer! Please, try again.");
     }
 }
