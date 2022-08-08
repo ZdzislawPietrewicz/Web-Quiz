9fb1f8dd1fdce7087f0017cb5a32d40577718028
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-08-08 12:08:26 +0200

    added model class User

---
+++ engine/model/User.java
@@ -0,0 +1,20 @@
+package engine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+    @Id
+    private String email;
+    private String password;
+}
